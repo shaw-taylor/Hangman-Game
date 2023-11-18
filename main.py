ecf8d5d57d0f2e06a5f84190d4ca263e8b168d38
Shaw <150867356+shaw-taylor@users.noreply.github.com>
2023-11-18 13:47:40 -0700

    main.py

--- main.py
+++ main.py
@@ -2,8 +2,7 @@
 
 import random
 
-#TODO-1: - Update the word list to use the 'word_list' from hangman_words.py
-#Delete this line: word_list = ["ardvark", "baboon", "camel"]
+# Update the word list to use the 'word_list' from hangman_words.py
 from hangman_words import word_list
 
 chosen_word = random.choice(word_list)
@@ -12,7 +11,7 @@ word_length = len(chosen_word)
 end_of_game = False
 lives = 6
 
-#TODO-3: - Import the logo from hangman_art.py and print it at the start of the game.
+# Import the logo from hangman_art.py and print it at the start of the game.
 from hangman_art import logo
 print(logo)
 
@@ -40,7 +39,7 @@ while not end_of_game:
 
     #Check if user is wrong.
     if guess not in chosen_word:
-        #TODO-5: - If the letter is not in the chosen_word, print out the letter and let them know it's not in the word.
+# If the letter is not in the chosen_word, print out the letter and let them know it's not in the word.
         print(f"You guessed {guess}, that's not in the word. You lose a life.")
         
         lives -= 1
@@ -56,6 +55,6 @@ while not end_of_game:
         end_of_game = True
         print("You win.")
 
-    #TODO-2: - Import the stages from hangman_art.py and make this error go away.
+  # Import the stages from hangman_art.py and make this error go away.
     from hangman_art import stages
     print(stages[lives])
